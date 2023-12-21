9d93d592b6132c228f248e4891b47a0bd323290d
vanek <ivan2005ab@gmail.com>
2023-12-21 17:04:55 +0300

    task0 done

---
+++ Task0.java
@@ -0,0 +1,12 @@
+//Настроить проект, вывести в консоль “Hello, world!”.
+//Вывести в консоль системные дату и время.
+
+import java.time.LocalDateTime;
+
+public class Task0 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println(LocalDateTime.now());
+    }
+
+}
