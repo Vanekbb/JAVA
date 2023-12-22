46552ee11d44ba2ab83d7477ebdc2dca402c7809
vanek <ivan2005ab@gmail.com>
2023-12-22 06:34:07 +0300

    GetNameTask done

---
+++ GetName.java
@@ -0,0 +1,17 @@
+//Написать программу, которая запросит пользователя ввести
+//<Имя> в консоли.
+//Получит введенную строку и выведет в консоль сообщение
+//“Привет, <Имя>!”
+
+import java.util.Scanner;
+
+public class GetName {
+    public static void main(String[] args) {
+        System.out.print("Введите имя: ");
+        Scanner scanner = new Scanner(System.in, "ibm866");
+        String name = scanner.nextLine();
+        System.out.println("Привет, " + name + "!");
+        System.out.printf("Привет, %s!", name);
+        scanner.close();
+    }
+}
